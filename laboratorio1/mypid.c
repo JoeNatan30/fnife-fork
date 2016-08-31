18fbfc4c4b557fc0163583eee1f0e7658540b906
JoeNatan30 <huamani.jn@pucp.edu.pe>
2016-08-31 01:34:54 -0500

    ordenación de documentos

--- laboratorio1/mypid.c
+++ laboratorio1/mypid.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(void){
     pid_t child;
     if(child=fork())
 	fprintf(stderr,"\nFork devolvio %d por tanto soy el padre con ID = %d",child,getpid());
-    else
+    else{
 	fprintf(stderr,"\nFork devolvio %d por tanto soy el hijo con ID = %d\n",child,getpid());
+	system("pstree > aprocesos.txt");
+    }
     return 0;
 }
